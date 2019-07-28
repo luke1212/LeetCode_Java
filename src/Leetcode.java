9bb2d01e1dd2d3f79db8c001ea509258daa1ffa7
Geng Sun <gsun@iastate.edu>
2019-07-27 23:53:09 -0500

    small change for gitkraken

--- src/Leetcode.java
+++ src/Leetcode.java
@@ -1,8 +1,16 @@
 public class Leetcode {
 
     public static void main(String[] args ){
-        System.out.print("Hi this is Java");
+        int x = 1;
+        int y = 4;
+        System.out.print(hammingDistance(x,y));
     }
 
-    
+    /* 461. Hamming Distance
+    * The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+    * Given two integers x and y, calculate the Hamming distance.
+    */
+    public static int hammingDistance(int x, int y) {
+        return 0;
+    }
 }
